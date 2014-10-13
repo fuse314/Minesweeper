8ffbaed793250cd5e2836be14e699af94d02e65b
fuse314 <github@gma.name>
2014-10-13 11:34:22 +0200

    board and field, first implementation

--- src/Field.java
+++ src/Field.java
@@ -1,4 +1,41 @@
 
 public class Field {
+	
+	private int _proximity;
+	public int getProximity() {
+		return this._proximity;
+	}
+	public void setProximity(int proximity) {
+		this._proximity = proximity;
+	}
+	
+	private FieldZustand _zustand;
+	public FieldZustand getZustand() {
+		return this._zustand;
+	}
+	public void setZustand(FieldZustand _zustand) {
+		this._zustand = _zustand;
+	}
 
+	private boolean _bombe;
+	public boolean isBombe() {
+		return _bombe;
+	}
+	public void setBombe(boolean _bombe) {
+		this._bombe = _bombe;
+	}
+	
+	public String toString(boolean showBombs) {
+		if(showBombs) {
+			if(_bombe) { return "X"; }
+		}
+		switch(_zustand) {
+		case Markiert:
+			return "M";
+		case Offen:
+			return Integer.toString(_proximity);
+		default:
+			return " ";
+		}
+	}
 }
